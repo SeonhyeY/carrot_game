896eb34df6a9153ab4e2c7e8908b525aede26496
SeonhyeY <syoon330@gmail.com>
2023-08-21 18:12:12 -0600

    Modify file locations in game.js

--- src/sound.js
+++ src/sound.js
@@ -1,8 +1,8 @@
-const bgSound = new Audio('../sound/bg.mp3');
-const carrotSound = new Audio('../sound/carrot_pull.mp3');
-const bugSound = new Audio('../sound/bug_pull.mp3');
-const winSound = new Audio('../sound/game_win.mp3');
-const alertSound = new Audio('../sound/alert.wav');
+const bgSound = new Audio('sound/bg.mp3');
+const carrotSound = new Audio('sound/carrot_pull.mp3');
+const bugSound = new Audio('sound/bug_pull.mp3');
+const winSound = new Audio('sound/game_win.mp3');
+const alertSound = new Audio('sound/alert.wav');
 
 export function playBg() {
   playSound(bgSound);
